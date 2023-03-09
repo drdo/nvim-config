a33fb879427b3f0e2c729e61b45c711adbd6195d
Daniel Rebelo de Oliveira <daniel.oliveira@intellecteu.com>
2023-03-09 15:36:20 +0000

    Sort options

--- init.lua
+++ init.lua
@@ -8,20 +8,20 @@ vim.g.loaded_python_provider = 0
 vim.g.loaded_python3_provider = 0
 
 vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.hlsearch = false
 vim.opt.clipboard = "unnamedplus" -- Make copy/paste use the system"s clipboard
+vim.opt.colorcolumn = { 80, 100 }
 vim.opt.completeopt = { "menu", "menuone" ,"preview", "noinsert", "noselect" }
 vim.opt.encoding = "utf-8"
 vim.opt.formatoptions:remove { "c", "r", "o" }
+vim.opt.hlsearch = false
 vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.smartcase = true
-vim.opt.termguicolors = true
-vim.opt.updatetime = 300
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = { 80, 100 }
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.updatetime = 300
 
 vim.keymap.set("n", "J", ":m >+1<CR>gv=gv")
 vim.keymap.set("n", "K", ":m >-2<CR>gv=gv")

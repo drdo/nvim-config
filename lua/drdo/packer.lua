7fb3419f0cadbee9ceaa31f4c17d4451265b091c
Daniel Rebelo de Oliveira <drdo@drdo.eu>
2023-03-22 18:58:34 +0000

    Add vim-surround

--- lua/drdo/packer.lua
+++ lua/drdo/packer.lua
@@ -65,6 +65,8 @@ require('packer').startup(function(use)
 
   use 'wellle/targets.vim' -- More targets for around/inside etc
 
+  use 'tpope/vim-surround' -- Change surroundings
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then

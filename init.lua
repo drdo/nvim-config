19b215a2cbe265be79c0fb02bb0ebc79851e72c7
Daniel Rebelo de Oliveira <drdo@drdo.eu>
2023-03-31 22:28:37 +0100

    Remove 80 lines column

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ vim.g.loaded_python3_provider = 0
 
 vim.opt.autoindent = true
 vim.opt.clipboard = "unnamedplus" -- Make copy/paste use the system"s clipboard
-vim.opt.colorcolumn = { 80, 100 }
+vim.opt.colorcolumn = { 100 }
 vim.opt.completeopt = { "menu", "menuone" ,"preview", "noinsert", "noselect" }
 vim.opt.encoding = "utf-8"
 vim.opt.formatoptions:remove { "c", "r", "o" }

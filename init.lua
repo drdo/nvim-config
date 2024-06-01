b66b9cc68b7e05544dd1ddfcd531e2f37f20b9cc
Daniel Rebelo de Oliveira <drdo@drdo.eu>
2024-06-01 14:45:58 +0100

    add autoread

--- init.lua
+++ init.lua
@@ -161,6 +161,9 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Reload files if they change outside vim
+vim.opt.autoread = true
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
